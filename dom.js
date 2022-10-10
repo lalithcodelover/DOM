1477ab3c4715fcea8493a578dcbb9107a650e44d
lalithcodelover <lalith.vlr@gmail.com>
2022-10-10 17:46:52 +0530

    elementbytagname added

--- dom.js
+++ dom.js
@@ -28,12 +28,22 @@
 // additem.style.fontWeight=500;
 // console.log('hello');
 
-var items = document.getElementsByClassName('list-group-item');
+// var items = document.getElementsByClassName('list-group-item');
 
-items[1].textContent='hello 2';
-//items[1].style.fontWeight ='bold';
-items[2].style.backgroundColor='green';
+// items[1].textContent='hello 2';
+// //items[1].style.fontWeight ='bold';
+// items[2].style.backgroundColor='green';
 
-for(var i=0;i<items.length;i++) {
-    items[i].style.fontWeight='bold';
+// for(var i=0;i<items.length;i++) {
+//     items[i].style.fontWeight='bold';
+// }
+
+var li = document.getElementsByTagName('li');
+
+li[1].textContent='hello 2';
+//li[1].style.fontWeight ='bold';
+li[2].style.backgroundColor='green';
+
+for(var i=0;i<li.length;i++) {
+    li[i].style.fontWeight='bold';
 }
